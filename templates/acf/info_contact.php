c1bf7ddcf3922350e592a6fabb5f5bf13e571708
Bruno Sertic <bruno.sertic@rokolabs.com>
2024-02-17 13:11:41 +0100

    info contact

---
+++ templates/acf/info_contact.php
@@ -0,0 +1,20 @@
+<section class="info-contact">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6 d-flex align-items-center">
+                <div class="row p-5">
+                    <div class="col-12 w-100">
+                        <h2 class="form-title"><?php echo $params['title']; ?></h2>
+                    </div>
+                    <div class="col-12 form-description w-100">
+                        <?php echo $params['description']; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 p-0">
+                <?php echo do_shortcode('[wpgmza id="1"]'); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
