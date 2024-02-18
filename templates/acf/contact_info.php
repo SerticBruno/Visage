c56b016bc6517fb458c0175f828eabaed9a98c17
Bruno Sertic <bruno.sertic@rokolabs.com>
2024-02-18 19:52:16 +0100

    Update contact_info.php

--- templates/acf/contact_info.php
+++ templates/acf/contact_info.php
@@ -13,7 +13,7 @@
             </div>
             <div class="col-12 col-md-6 p-0">
                 <?php echo do_shortcode('[wpgmza id="1"]'); ?>
-                <!-- test1 -->
+                <!-- test3 -->
             </div>
         </div>
     </div>
