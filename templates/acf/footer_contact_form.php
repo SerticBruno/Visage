0abd6cd3cd4c163d4171f8019c9f01749acde515
Bruno Sertic <bruno.sertic@rokolabs.com>
2024-04-04 21:37:51 +0200

    contact

--- templates/acf/footer_contact_form.php
+++ templates/acf/footer_contact_form.php
@@ -5,12 +5,15 @@
 <section class="footer-contact-form">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="form-description"><?php echo $footer['footer_contact_description']; ?></div>
+            <div class="col-12 col-md-6 d-flex justify-content-center">
+                <div class="form-description w-90">
+                    <?php echo $footer['footer_contact_description']; ?>
+                </div>
             </div>
-            <div class="col-12 col-md-6 footer-contact-form-visage">
-                <?php //echo do_shortcode('[forminator_form id="429"]'); ?>
-                <?php echo do_shortcode('[forminator_form id="240"]'); ?>
+            <div class="col-12 col-md-6 d-flex justify-content-center footer-contact-form-visage">
+                <div class="w-90">
+                    <?php echo do_shortcode('[forminator_form id="240"]'); ?>
+                </div>
             </div>
         </div>
     </div>
