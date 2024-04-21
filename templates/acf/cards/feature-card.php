6ec5b1731a47d9817c5248b741acfe64fded50bd
Bruno Sertic <sertic.bruno@gmail.com>
2024-04-21 16:17:18 +0200

    features

--- templates/acf/cards/feature-card.php
+++ templates/acf/cards/feature-card.php
@@ -8,14 +8,16 @@ $description = $params['description'];
 
 ?>
 
-<div class="col-lg-3 pb-5 link-wrapper">
+<div class="col-lg-3 col-md-4 col-6 pb-5 link-wrapper">
     <a href="<?php echo get_permalink($post); ?>" class="position-relative">
         <div class="features-card">
             <div class="img-wrapper">
                 <img src="<?php echo $image ?>"></img>
             </div>
-            <div class="info">
+            <div class="title">
                 <h4><?php echo $title ?></h4>
+            </div>
+            <div class="info">
                 <p><?php echo $description ?></p>
             </div>
         </div>
