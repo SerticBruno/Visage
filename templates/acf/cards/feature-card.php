3674e4bd1b26ae6c207ae7ff206eeccaed0afe24
Bruno Sertić <bruno.sertic@rokolabs.com>
2024-04-21 10:12:27 +0200

    wip

---
+++ templates/acf/cards/feature-card.php
@@ -0,0 +1,23 @@
+<?php 
+    
+$feature = $params;
+$link = $feature['url'];
+$title = $params['title'];
+$image = $params['image']['url'];
+$description = $params['description'];
+
+?>
+
+<div class="col-lg-3 pb-5 link-wrapper">
+    <a href="<?php echo get_permalink($post); ?>" class="position-relative">
+        <div class="features-card">
+            <div class="img-wrapper">
+                <img src="<?php echo $image ?>"></img>
+            </div>
+            <div class="info">
+                <h4><?php echo $title ?></h4>
+                <p><?php echo $description ?></p>
+            </div>
+        </div>
+    </a>
+</div>
--- templates/acf/price_list.php
+++ templates/acf/price_list.php
@@ -7,7 +7,7 @@ $content = $params['content'];
 <section class="price-list py-4">
     <div class="container py-3 versailles">
         <div class="row">
-            <div class="col-12 col-lg-2 pb-4">
+            <div class="col-12 col-lg-2 p-0 pb-4">
                 <h2>Cjenik</h2>
             </div>
             <div class="col-12 col-lg-10">
@@ -15,13 +15,30 @@ $content = $params['content'];
                     <div class="category-section">
                         <div class="category-row row">
                             <div class="category-title col-12 strong">
-                                <?php echo htmlspecialchars($category['title']); ?>
+                                <?php 
+                                    // Check if a link anchor is provided and not empty
+                                    if (!empty($category['link_anchor'])) {
+                                        // Append the anchor to the URL
+                                        $urlLink = $category['category_link'] . '#' . $category['link_anchor'];
+                                    } else {
+                                        // Use the basic URL without an anchor
+                                        $urlLink = $category['category_link'];
+                                    }
+                                ?>
+                                <!-- Create the anchor tag with the URL -->
+                                <a href="<?php echo htmlspecialchars($urlLink); ?>">
+                                    <?php echo htmlspecialchars($category['title']); ?>
+                                </a>
+
                             </div>
                         </div>
                         <?php foreach($category['products'] as $product): ?>
                             <div class="product-row row">
                                 <div class="col-6 product-title">
-                                    <?php echo htmlspecialchars($product['title']); ?>
+                                    <?php echo htmlspecialchars($product['title']); ?><br>
+                                    <span class="product-description notoserifdisplay">
+                                        <?php echo htmlspecialchars($product['description']); ?>
+                                    </span>
                                 </div>
                                 <div class="col-6 product-price">
                                     <?php echo htmlspecialchars($product['price']); ?> EUR
