59ade085f70b697f098e3517db298a9c5d746fe9
Bruno Sertic <bruno.sertic@rokolabs.com>
2024-05-26 11:19:03 +0200

    fx

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@ defined( 'ABSPATH' ) || exit;
 define('THEME_DIR', get_stylesheet_directory());
 define('THEME_URL', get_stylesheet_directory_uri());
 // define('THEME_OPTIONS', get_fields('option'));
+$theme = wp_get_theme();
+define('THEME_VERSION', $theme->Version); //gets version written in your style.css
 
 require THEME_DIR . '/inc/enqueue.php';
 require THEME_DIR . '/inc/util.php';
