d18494ffa142ecbe90c60fac7acccbf383e4953e
Bruno Sertić <bruno.sertic@rokolabs.com>
2024-07-05 00:39:21 +0200

    ide

--- templates/acf/image_text.php
+++ templates/acf/image_text.php
@@ -2,7 +2,7 @@
 ?>
 
 <section class="image-text" id="<?php echo $anchor ?>">
-    <div class="container-fluid p-0 m-0">
+    <div class="container-fluid">
         <?php foreach($params['rows'] as $k => $v) { ?>
             <?php
                 $image = $v['image']['url'];
@@ -13,23 +13,25 @@
                 $anchor = $v['anchor'];
 
             ?>
+            <div class="row py-5 justify-content-center anchor-target <?php echo $k % 2 == 0 ? 'dark-gray' : 'gray'; ?>" id="<?php echo $anchor ?>">
+                <div class="col-11 d-flex">
+                    <div class="row <?php echo $k % 2 == 1 ? 'flex-row-reverse' : ''; ?>">
 
-            <div class="row justify-content-center px-1 m-0 py-5 py-lg-5 anchor-target <?php echo $k % 2 == 0 ? 'dark-gray' : 'gray'; ?>" id="<?php echo $anchor ?>">
-                <div class="row content px-1 px-md-4 px-lg-3 <?php echo $k % 2 == 1 ? 'flex-row-reverse' : ''; ?>">
-                    <div class="col-lg-8 col-12 ">
-                        <div class="text">
-                            <?php echo $description ?>
-
-                            <?php if(!empty($button['label'])){ ?>
-                                <div class="button-wrapper pb-4 pb-lg-0">
-                                    <?php include THEME_DIR . '/templates/acf/button-show.php'; ?>
-                                </div>
-                            <?php } ?>
+                        <div class="col-md-8 col-12">
+                            <div class="text">
+                                <?php echo $description ?>
+                                
+                                <?php if(!empty($button['label'])){ ?>
+                                    <div class="button-wrapper pb-4 pb-lg-0">
+                                        <?php include THEME_DIR . '/templates/acf/button-show.php'; ?>
+                                    </div>
+                                <?php } ?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-12 d-flex justify-content-center">
-                        <div class="img-wrapper">
-                            <img src="<?php echo $image ?>" alt="<?php echo $alt ?>">
+                        <div class="col-md-4 col-12">
+                            <div class="img-wrapper">
+                                <img src="<?php echo $image ?>" alt="<?php echo $alt ?>">
+                            </div>
                         </div>
                     </div>
                 </div>
